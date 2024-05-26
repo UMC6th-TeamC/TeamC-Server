10f0c3714a712656e37a1a087a0e1380ce6d5780
chamm99 <alslalsl3125@gmail.com>
2024-05-27 02:12:35 +0900

    :sparkles: feat: initiate project - gitignore 설정

---
+++ src/test/java/com/umc/teamB/TeambApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.teamB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeambApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
