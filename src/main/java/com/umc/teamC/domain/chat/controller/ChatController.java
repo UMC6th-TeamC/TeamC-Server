e14bdb8bd581eacea28f7be7338f667c42db6984
chamm99 <alslalsl3125@gmail.com>
2024-06-22 17:31:25 +0900

    chat update

--- src/main/java/com/umc/teamC/domain/chat/controller/ChatController.java
+++ src/main/java/com/umc/teamC/domain/chat/controller/ChatController.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,18 +21,23 @@ public class ChatController {
 
     private final ChatService chatService;
 
-    //메세지 보내기 컨트롤러
-    @MessageMapping("/chat.message")
-    @SendTo("/topic/public")
-    public Map<String, Object> sendMessage(Map<String, Object> message) {
-        // 메세지 확인 로그
-        log.info(String.valueOf(message));
+    // 메시지 보내기 컨트롤러
+    @MessageMapping("/chat.message/{chatRoomId}")
+    @SendTo("/topic/{chatRoomId}")
+    public Map<String, Object> sendMessage(
+            @PathVariable Long chatRoomId,
+            Map<String, Object> message) {
+        // 메시지 확인 로그
+        log.info("Received message: {}", message);
         String sender = message.get("sender").toString();
         String content = message.get("content").toString();
+
         Chat message1 = Chat.builder()
                 .sender(sender)
                 .content(content)
+                .chatId(chatRoomId)
                 .build();
+
         Chat chat = chatService.saveMessage(message1);
         Map<String, Object> result = new HashMap<>();
         result.put("id", chat.getChatId());
@@ -40,9 +46,10 @@ public class ChatController {
         return result;
     }
 
-    @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
-    public Chat addUser(Chat chat) {
+    @MessageMapping("/chat.addUser/{chatRoomId}")
+    @SendTo("/topic/{chatRoomId}")
+    public Chat addUser(@PathVariable Long chatRoomId, Chat chat) {
+        chat.setId(chatRoomId);
         return chat;
     }
 }
